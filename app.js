911ebed97b4261c8dbecada81a03224ca57da5fc
Jordan Dwyer <dwyer.jordan9@gmail.com>
2022-11-06 18:40:24 -0500

    Exercise 3

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ window.addEventListener('load',() =>{
     document.querySelector("button").addEventListener("click", (event) =>{
         event.preventDefault();
 
-        let result = document.querySelector();
+        let result = document.querySelector("div#result");
 
         fetch(`superheroes.php?query= ${document.querySelector("input#searchbar").value.replace(/[-&\/\\#,+()$@|~%!.'":;*?<>{}]/g,'')}`.replace(/"[^-0-9+&@#/%?=~_|!:,.;\(\)]"/g, ''), { method: 'GET' })
-        .then(resp => resp.text())
-        .then(data => {
-
-            let e1 = document.createElement("h2");
-            e1.appendChild(document.createTextNode("RESULT"));
-            let e2 = document.createElement("hr");
-            result.innerHTML = '';
-            result.innerHTML = data;
-            result.prepend(e1, e2)
-        })
-    
+            .then(resp => resp.text())
+            .then(data => {
+                let e1 = document.createElement("h2");
+                e1.appendChild(document.createTextNode("RESULT"));
+                let e2 = document.createElement("hr");
+                result.innerHTML = '';
+                result.innerHTML = data;
+                result.prepend(e1, e2)
+            })
     });
+
 });
